f574f60b4c505af85931c0255e6bf520437330d0
atifkarim <atif0902082@gmail.com>
2020-05-01 00:00:50 +0200

    hsv problem solved

--- img_process.py
+++ img_process.py
@@ -4,7 +4,8 @@ import cv2
 
 def do_gray(frame):
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     return hsv
+    
 
 # This change I have done after adding submodule from my parent repo
