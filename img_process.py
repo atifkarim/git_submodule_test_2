595a24076fb892ca0ebfc5cf0114d7607be74ad2
atifkarim <atif0902082@gmail.com>
2020-04-30 23:56:38 +0200

    commited from master repo git_submodule_test_2

--- img_process.py
+++ img_process.py
@@ -4,12 +4,7 @@ import cv2
 
 def do_gray(frame):
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-<<<<<<< HEAD
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     return hsv
-=======
-    return img_gray
-
 
 # This change I have done after adding submodule from my parent repo
->>>>>>> d2450deb844925e60a45ea766b47c5b5b0d2964a
