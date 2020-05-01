2e0fb367e1acf81436f1fc8405fd4bc6d82ed1fd
atifkarim <atif0902082@gmail.com>
2020-05-01 02:08:41 +0200

    text 2

--- img_process.py
+++ img_process.py
@@ -4,9 +4,10 @@ import cv2
 
 def do_gray(frame):
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    return hsv
+    hsv_1 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    return hsv_1
     
 
 # This change I have done after adding submodule from my parent repo
 # total headbang stuffs
+# changed now
